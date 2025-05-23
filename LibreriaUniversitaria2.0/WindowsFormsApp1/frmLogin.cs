31607379ed349682e9458611d7b0a11f9a408c72
diego <diegocesari22@gmail.com>
2025-05-23 14:16:12 -0300

    diego
    nuevo

---
+++ LibreriaUniversitaria2.0/WindowsFormsApp1/frmLogin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblUsuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+           string usuario =textBox1.Text;
+           string password = textBox2.Text;
+            if(usuario == "" || password == "")
+            {
+                MessageBox.Show("Rellene todos los campos por favor");
+                return;
+            }
+            else
+            {
+                if(usuario =="diego" && password == "123")
+                {
+                    MessageBox.Show("Bienvenido" + textBox1.Text+ "Usuario registrado");
+                }
+                else
+                {
+                    if (usuario == "esmilce" && password == "123")
+                    {
+                        MessageBox.Show("Bienvenido" + textBox1.Text + "Usuario registrado");
+                    }
+                    else
+                    {
+                        MessageBox.Show("usuario no registrado");
+                        return;
+                    }
+                }
+
+            }
+        }
+        private void frm_closing(object sender, FormClosingEventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
---
+++ LibreriaUniversitaria2.0/WindowsFormsApp1/frmMenu.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVenta = new System.Windows.Forms.Button();
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.btnStock = new System.Windows.Forms.Button();
+            this.btnABM = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnReserva = new System.Windows.Forms.Button();
+            this.btnCompras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVenta
+            // 
+            this.btnVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVenta.Location = new System.Drawing.Point(74, 112);
+            this.btnVenta.Name = "btnVenta";
+            this.btnVenta.Size = new System.Drawing.Size(118, 59);
+            this.btnVenta.TabIndex = 0;
+            this.btnVenta.Text = "Realizar una Venta";
+            this.btnVenta.UseVisualStyleBackColor = true;
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.AutoSize = true;
+            this.lblMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenu.Location = new System.Drawing.Point(287, 43);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(217, 32);
+            this.lblMenu.TabIndex = 1;
+            this.lblMenu.Text = "Menu Principal";
+            // 
+            // btnStock
+            // 
+            this.btnStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStock.Location = new System.Drawing.Point(344, 112);
+            this.btnStock.Name = "btnStock";
+            this.btnStock.Size = new System.Drawing.Size(118, 59);
+            this.btnStock.TabIndex = 2;
+            this.btnStock.Text = "Consultar Stock";
+            this.btnStock.UseVisualStyleBackColor = true;
+            // 
+            // btnABM
+            // 
+            this.btnABM.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnABM.Location = new System.Drawing.Point(590, 112);
+            this.btnABM.Name = "btnABM";
+            this.btnABM.Size = new System.Drawing.Size(118, 59);
+            this.btnABM.TabIndex = 3;
+            this.btnABM.Text = "ABM Usuarios";
+            this.btnABM.UseVisualStyleBackColor = true;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.Location = new System.Drawing.Point(74, 218);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(118, 59);
+            this.btnReportes.TabIndex = 4;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.UseVisualStyleBackColor = true;
+            // 
+            // btnReserva
+            // 
+            this.btnReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReserva.Location = new System.Drawing.Point(344, 218);
+            this.btnReserva.Name = "btnReserva";
+            this.btnReserva.Size = new System.Drawing.Size(130, 59);
+            this.btnReserva.TabIndex = 5;
+            this.btnReserva.Text = "Realizar una Reserva";
+            this.btnReserva.UseVisualStyleBackColor = true;
+            // 
+            // btnCompras
+            // 
+            this.btnCompras.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompras.Location = new System.Drawing.Point(590, 218);
+            this.btnCompras.Name = "btnCompras";
+            this.btnCompras.Size = new System.Drawing.Size(118, 59);
+            this.btnCompras.TabIndex = 6;
+            this.btnCompras.Text = "Compras";
+            this.btnCompras.UseVisualStyleBackColor = true;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCompras);
+            this.Controls.Add(this.btnReserva);
+            this.Controls.Add(this.btnReportes);
+            this.Controls.Add(this.btnABM);
+            this.Controls.Add(this.btnStock);
+            this.Controls.Add(this.lblMenu);
+            this.Controls.Add(this.btnVenta);
+            this.Name = "frmMenu";
+            this.Text = "frmMenu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVenta;
+        private System.Windows.Forms.Label lblMenu;
+        private System.Windows.Forms.Button btnStock;
+        private System.Windows.Forms.Button btnABM;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnReserva;
+        private System.Windows.Forms.Button btnCompras;
+    }
+}
