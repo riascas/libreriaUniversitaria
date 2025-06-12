d840eeb9b28c5f2a16031ffc77ccccb41602758a
Esmilce Mendoza <esmilceupe@gmail.com>
2025-06-12 09:07:19 -0300

    Agregado App.config y clase DbHelper.cs para manejar la cadena de conexión

---
+++ LibreriaUniversitaria2.0/Datos/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Libreria.DAL
+{
+    /// <summary>
+    /// Clase auxiliar para acceder a la cadena de conexión definida en App.config
+    /// </summary>
+    public static class DbHelper
+    {
+        public static string CadenaConexion
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            }
+        }
+    }
+}
