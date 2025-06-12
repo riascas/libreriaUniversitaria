d5a9f9bd41edc611735c10f3a692382405acbde7
Esmilce Mendoza <esmilceupe@gmail.com>
2025-06-12 09:20:16 -0300

    Agregado LibroRepository.cs con métodos CRUD completos y conectados a la base de datos

--- LibreriaUniversitaria2.0/Datos/LibroRepository.cs
+++ LibreriaUniversitaria2.0/Datos/LibroRepository.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LibreriaUniversitaria.Entidades;
 using System.Data.SqlClient;
-
+using LibreriaUniversitaria.Entidades;
 
 namespace Libreria.DAL
 {
