61ec409dbaaad849125aec552bbd5dec5f32b74a
Alex Menorque <alex.menorque@gmail.com>
2025-06-30 21:56:46 -0300

    Subo unos cambios en la capa de datos

--- LibreriaUniversitaria2.0/DAT-Libreria/ClienteDAO.cs
+++ LibreriaUniversitaria2.0/DAT-Libreria/ClienteDAO.cs
@@ -16,11 +16,24 @@ namespace DAT_Libreria
         public List<Cliente> ObtenerTodos()
         {
             List<Cliente> lista = new List<Cliente>();
-            DataTable tabla = conexion.LeerPorComando("SELECT * FROM Cliente");
+
+            string consulta = @"
+            SELECT c.idCliente, c.Estudiante,
+                   p.idPersona, p.Nombre, p.Apellido, p.DNI, p.Email,
+                   d.idDomicilio, d.Calle, d.Altura,
+                   l.idLocalidad, l.Descripcion AS NombreLocalidad,
+                   m.idMunicipio, m.NombreMunicipio
+            FROM Cliente c
+            INNER JOIN Persona p ON c.FK_Persona = p.idPersona
+            INNER JOIN Domicilio d ON p.FK_Domicilio = d.idDomicilio
+            INNER JOIN Localidad l ON d.FK_Localidad = l.idLocalidad
+            INNER JOIN Municipio m ON l.FK_Municipio = m.idMunicipio";
+
+            DataTable tabla = conexion.LeerPorComando(consulta);
 
             foreach (DataRow fila in tabla.Rows)
             {
-                lista.Add(new Cliente
+                Cliente cliente = new Cliente
                 {
                     IdCliente = Convert.ToInt32(fila["idCliente"]),
                     EsEstudiante = Convert.ToBoolean(fila["EsEstudiante"]),
@@ -31,17 +44,39 @@ namespace DAT_Libreria
                         Apellido = fila["Apellido"].ToString(),
                         DNI = fila["DNI"].ToString(),
                         Email = fila["Email"].ToString(),
-                        
+                        UnDomicilio = new Domicilio
+                        {
+                            IdDomicilio = Convert.ToInt32(fila["idDomicilio"]),
+                            Calle = fila["Calle"].ToString(),
+                            Altura = Convert.ToInt32(fila["Altura"]),
+                            UnaLocalidad = new Localidad
+                            {
+                                IdLocalidad = Convert.ToInt32(fila["idLocalidad"]),
+                                NombreLocalidad = fila["NombreLocalidad"].ToString(),
+                                UnMunicipio = new Municipio
+                                {
+                                    IdMunicipio = Convert.ToInt32(fila["idMunicipio"]),
+                                    NombreMunicipio = fila["NombreMunicipio"].ToString()
+                                }
+                            }
+                        }
                     }
-                });
+                };
+
+                lista.Add(cliente);
             }
+
             return lista;
         }
 
-        public int Insertar(Cliente cliente)
+        public int InsertarCliente(Cliente cliente)
         {
-            string query = $"INSERT INTO Cliente (EsEstudiante, UnaPersona) VALUES ('{(cliente.EsEstudiante ? 1 : 0)}', {cliente.UnaPersona})";
+            string query = $@"
+            INSERT INTO Cliente (EsEstudiante, FK_Persona)
+            VALUES ('{(cliente.EsEstudiante ? 1 : 0)}', {cliente.UnaPersona.IdPersona})";
+
             return conexion.EscribirPorComando(query);
         }
     }
+
 }
--- LibreriaUniversitaria2.0/DAT-Libreria/EmpleadoDAO.cs
+++ LibreriaUniversitaria2.0/DAT-Libreria/EmpleadoDAO.cs
@@ -16,11 +16,26 @@ namespace DAT_Libreria
         public List<Empleado> ObtenerTodos()
         {
             List<Empleado> lista = new List<Empleado>();
-            DataTable tabla = conexion.LeerPorComando("SELECT * FROM Empleado");
+
+            string consulta = @"
+            SELECT e.idEmpleado, e.Usuario, e.Clave,
+                   p.idPersona, p.Nombre, p.Apellido, p.DNI, p.Email,
+                   d.idDomicilio, d.Calle, d.Altura,
+                   l.idLocalidad, l.Descripcion AS NombreLocalidad,
+                   m.idMunicipio, m.NombreMunicipio,
+                   r.idRolEmpleado, r.Rol
+            FROM Empleado e
+            INNER JOIN Persona p ON e.FK_Persona = p.idPersona
+            INNER JOIN Domicilio d ON p.FK_Domicilio = d.idDomicilio
+            INNER JOIN Localidad l ON d.FK_Localidad = l.idLocalidad
+            INNER JOIN Municipio m ON l.FK_Municipio = m.idMunicipio
+            INNER JOIN RolEmpleado r ON e.FK_RolEmpleado = r.idRolEmpleado";
+
+            DataTable tabla = conexion.LeerPorComando(consulta);
 
             foreach (DataRow fila in tabla.Rows)
             {
-                lista.Add(new Empleado
+                Empleado emp = new Empleado
                 {
                     IdEmpleado = Convert.ToInt32(fila["idEmpleado"]),
                     Usuario = fila["Usuario"].ToString(),
@@ -36,11 +51,39 @@ namespace DAT_Libreria
                         Nombre = fila["Nombre"].ToString(),
                         Apellido = fila["Apellido"].ToString(),
                         DNI = fila["DNI"].ToString(),
-                        Email = fila["Email"].ToString()
+                        Email = fila["Email"].ToString(),
+                        UnDomicilio = new Domicilio
+                        {
+                            IdDomicilio = Convert.ToInt32(fila["idDomicilio"]),
+                            Calle = fila["Calle"].ToString(),
+                            Altura = Convert.ToInt32(fila["Altura"]),
+                            UnaLocalidad = new Localidad
+                            {
+                                IdLocalidad = Convert.ToInt32(fila["idLocalidad"]),
+                                NombreLocalidad = fila["NombreLocalidad"].ToString(),
+                                UnMunicipio = new Municipio
+                                {
+                                    IdMunicipio = Convert.ToInt32(fila["idMunicipio"]),
+                                    NombreMunicipio = fila["NombreMunicipio"].ToString()
+                                }
+                            }
+                        }
                     }
-                });
+                };
+
+                lista.Add(emp);
             }
+
             return lista;
+        }
+
+        public int InsertarEmpleado(Empleado emp)
+        {
+            string query = $@"
+            INSERT INTO Empleado (Usuario, Clave, FK_Persona, FK_RolEmpleado)
+            VALUES ('{emp.Usuario}', '{emp.Clave}', {emp.UnaPersona.IdPersona}, {emp.UnRol.IdRolEmpleado})";
+
+            return conexion.EscribirPorComando(query);
         }
     }
 }
